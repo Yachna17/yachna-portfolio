037314614438bc27e8ce5c9a7eaf341f0c9e89d5
Yachna <yachnarupwal@gmail.com>
2026-04-17 01:22:45 +0530

    feat: add vercel analytics

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import { ThemeProvider } from './context/ThemeContext'
 import './index.css'
 import App from './App.jsx'
+import { inject } from '@vercel/analytics'
+inject()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
