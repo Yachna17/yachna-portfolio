20a4fc88e9fb37932aa84522eb5dfc855b3c68f9
Yachna <yachnarupwal@gmail.com>
2026-05-09 18:59:54 +0530

    fix: force IPv4 on nodemailer SMTP to resolve ENETUNREACH

--- server/controllers/contactController.js
+++ server/controllers/contactController.js
@@ -10,7 +10,10 @@ exports.send = async (req, res) => {
     console.log("Message saved to DB");
 
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+      host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    family: 4,    
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
